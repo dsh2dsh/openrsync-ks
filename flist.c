dsh2dsh/openrsync-ks

commit e95ae8741efc0a79ddb9d7599650b606b2af6c35

openrsync: don't truncate input missing EOL prematurely
fdgets() assumes that every file in the listing will be either newline or
NUL terminated, depending on the options used.  However, it misses that a
filename at EOF doesn't need to be terminated- we still need to pick it up.

Just break and return the current length once we hit EOF, instead of
truncating the listing.

flist.c
@@ -3139,7 +3139,7 @@ fdgets(struct sess *sess, int fd, char *buf, size_t bufsz)
 		}
 
 		if (n == 0)
-			return 0;
+			break;
 
 		length += n;
 

tests/src/test74_filesfrom.test
@@ -743,3 +743,10 @@ echo "Test 26.3"
 rm -rf dst
 rsync -a --from0 --files-from=- < ffdirs0 -- ${SRC}/ ${DST}/
 compare_trees src dst
+
+echo "Test 27"
+1>&2 echo "Test 27"
+rm -rf dst
+xfer=$(head -1 fffiles)
+echo "$xfer" | tr -d $'\n' | rsync -a --files-from=- -- ${SRC}/ ${DST}/
+cmp -s src/"$xfer" dst/"$xfer"

commit 1648e9ebd37a75127fde208d0b07c868717e5b38

openrsync: ensure that fdgets() properly terminates the buffer
The main problem here was introduced in
e95ae8741 (openrsync: don't truncate input missing EOL prematurely)
but the secondary problem that we'll actually read up to the full buffer
size was noticed while working on this patch.

Rewrite fdgets() to be a little more straightforward to read through; we now
read the single byte onto the stack, then interpret it and optionally append
it to the buffer.  fdgets() hasn't been able to read more than a single byte
at a time in a long time to avoid over-reading, so it seems better to avoid
that pretense entirely.

flist.c
@@ -3128,31 +3128,35 @@ fdgets(struct sess *sess, int fd, char *buf, size_t bufsz)
 	size_t length = 0;
 	ssize_t n;
 
-	while (length < bufsz) {
-		n = read(fd, buf + length, 1);
+	while (length < bufsz - 1) {
+		char readc;
+
+		n = read(fd, &readc, 1);
 		if (n == -1) {
 			if (errno == EAGAIN || errno == EINTR)
 				continue;
 
 			ERR("read(2) of files-from file failed");
 			return -1;
-		}
-
-		if (n == 0)
-			break;
-
-		length += n;
-
-		if (*(buf + length - 1) == '\0')
-			break;
-		if (!sess->opts->from0 &&
-		    (*(buf + length - 1) == '\n' ||
-		     *(buf + length - 1) == '\r')) {
-			*(buf + length - 1) = '\0';
+		} else if (n == 0) {
+			if (length > 0)
+				buf[length++] = '\0';
 			break;
 		}
+
+		if (!sess->opts->from0 && (readc == '\n' || readc == '\r')) {
+			buf[length++] = '\0';
+			break;
+		}
+
+		buf[length++] = readc;
+		if (readc == '\0')
+			break;
 	}
 
+	if (length == bufsz - 1)
+		buf[length++] = '\0';
+	assert(length == 0 || buf[length - 1] == '\0');
 	return length;
 }
 

tests/src/test74_filesfrom.test
@@ -750,3 +750,15 @@ rm -rf dst
 xfer=$(head -1 fffiles)
 echo "$xfer" | tr -d $'\n' | rsync -a --files-from=- -- ${SRC}/ ${DST}/
 cmp -s src/"$xfer" dst/"$xfer"
+
+echo "Test 27.1"
+1>&2 echo "Test 27.1"
+rm -rf dst
+# This differs from the previous test in that it confirms that rsync is properly
+# nul-terminating the second entry that didn't have any EOL marker.
+flong="$DIRBASE-1/$DIRBASE-1/$FILEBASE-2"
+fshort="$DIRBASE-1/$FILEBASE-2"
+printf "$flong\n$fshort" | rsync -a --files-from=- -- ${SRC}/ ${DST}/
+for target in $flong $fshort; do
+	cmp -s src/"$target" dst/"$target"
+done