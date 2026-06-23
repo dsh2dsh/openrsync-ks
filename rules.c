bc3d8cb02b1c4150621942fa8648314cdc89ad6f
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-06-23 17:19:34 +0200

    Add more version conrols into CVS exclude rules
    This change syncs CVS exclude rules with original rsync.

--- rules.c
+++ rules.c
@@ -108,7 +108,7 @@ static const char builtin_cvsignore[] =
 	"_$* *$ *.old *.bak *.BAK *.orig *.rej .del-* "
 	"*.a *.olb *.o *.obj *.so *.exe *.Z *.elc "
 	"*.ln core "
-	".svn/ ";
+	".svn/ .git/ .hg/ .bzr/ ";
 
 static char		 rule_base[MAXPATHLEN];
 static char		*rule_base_cwdend;
